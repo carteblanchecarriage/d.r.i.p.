afb4e62d15d532cb7347fb087b79fc48cb247e02
carteblanchecarriage <115643776+carteblanchecarriage@users.noreply.github.com>
2022-10-28 12:50:14 -0400

    update

--- script.js
+++ script.js
@@ -1,10 +1,30 @@
-const roofWidth = document.getElementById('roofWidth').value;
-const length = document.getElementById('length');
-const output1 = document.getElementById('output1');
-const subBtn = document.getElementById('submitBtn');
+/*const navMenu = document.getElementById("nav-menu");
+const hamburger = document.getElementById("hamburger");
 
-function area() {
-    output1.innerHTML = roofWidth.value;
+hamburger.addEventListener("click", expandMenu);
+
+function expandMenu() {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");\drip\index.html
 }
 
-subBtn.addEventListener("click", area);
+
+const navMenu = document.getElementById("nav-menu");
+const hamburger = document.getElementById("hamburger");
+*/
+
+
+const roofWidth = document.getElementById("roofWidth");
+const roofLength = document.getElementById("roofLength");
+const rainfall = document.getElementById("rainfall");
+const gutterLength = document.getElementById("gutterLength");
+const outputText = document.getElementById('outputText');
+const form1 = document.getElementById('form1');
+const submitBtn = document.getElementById("submitBtn");
+
+function gutterCalc() {
+    event.preventDefault();
+    outputText.innerHTML = (roofWidth.value * roofLength.value) + " inches squared";
+}
+
+submitBtn.addEventListener('click', gutterCalc);
