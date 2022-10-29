12a82183c5be0210926ecbafa1ae7fa355af8073
carteblanchecarriage <115643776+carteblanchecarriage@users.noreply.github.com>
2022-10-29 14:19:18 -0400

    Update

--- script.js
+++ script.js
@@ -18,13 +18,20 @@ const roofWidth = document.getElementById("roofWidth");
 const roofLength = document.getElementById("roofLength");
 const rainfall = document.getElementById("rainfall");
 const gutterLength = document.getElementById("gutterLength");
-const outputText = document.getElementById('outputText');
+const outputText = +document.getElementById('outputText');
 const form1 = document.getElementById('form1');
 const submitBtn = document.getElementById("submitBtn");
+const drainedArea = roofWidth * roofLength;
+const ratioM = .75;
+//let gutterWidth = 0.016 * (Math.pow(ratioM, (-4 / 7))) * Math.pow(gutterLength.value, (3 / 28)) * Math.pow((rainfall.value * drainedArea), (5 / 14));
+const gutterWidth = roofWidth;
 
 function gutterCalc() {
-    event.preventDefault();
-    outputText.innerHTML = (roofWidth.value * roofLength.value) + " inches squared";
+    //event.preventDefault();
+    outputText.innerHTML = 5;
 }
 
 submitBtn.addEventListener('click', gutterCalc);
+
+// Gutter Calculator
+// https://www.metalsales.us.com/wp-content/uploads/2020/01/1b-general-information-pm-2018.pdf
