1c0f3646f02ddd6ca10660b001ebcc427e34810d
carteblanchecarriage <115643776+carteblanchecarriage@users.noreply.github.com>
2022-11-03 14:57:13 -0400

    Updates
    got the hamburger working finally!

--- script.js
+++ script.js
@@ -1,37 +1,35 @@
-/*const navMenu = document.getElementById("nav-menu");
-const hamburger = document.getElementById("hamburger");
-
-hamburger.addEventListener("click", expandMenu);
-
-function expandMenu() {
-    hamburger.classList.toggle("active");
-    navMenu.classList.toggle("active");\drip\index.html
-}
-
+/* <<< THIS IS THE HAMBURGER EFFECT >>> */
 
 const navMenu = document.getElementById("nav-menu");
 const hamburger = document.getElementById("hamburger");
-*/
 
+hamburger.addEventListener('click', function () {
+    const result1 = hamburger.classList.toggle("active");
+    const result2 = navMenu.classList.toggle("active");
+});
+
+
+/* <<< THIS IS THE GUTTER CALCULATION >>> */
 
 const roofWidth = document.getElementById("roofWidth");
 const roofLength = document.getElementById("roofLength");
 const rainfall = document.getElementById("rainfall");
 const gutterLength = document.getElementById("gutterLength");
-const outputText = +document.getElementById('outputText');
+const outputText1 = document.getElementById('outputText1');
+const outputText2 = document.getElementById('outputText2')
 const form1 = document.getElementById('form1');
 const submitBtn = document.getElementById("submitBtn");
-const drainedArea = roofWidth * roofLength;
-const ratioM = .75;
-//let gutterWidth = 0.016 * (Math.pow(ratioM, (-4 / 7))) * Math.pow(gutterLength.value, (3 / 28)) * Math.pow((rainfall.value * drainedArea), (5 / 14));
-const gutterWidth = roofWidth;
+const drainedArea = roofWidth.value * roofLength.value;
+const ratioM = document.getElementById("ratioM");
+const gutterWidth = 0.016 * (Math.pow(ratioM.value, (-4 / 7))) * Math.pow(gutterLength.value, (3 / 28)) * Math.pow((rainfall.value * (roofWidth.value * roofLength.value)), (5 / 14));
 
-function gutterCalc() {
-    //event.preventDefault();
-    outputText.innerHTML = 5;
-}
+submitBtn.addEventListener('click', function gutterCalc() {
+    const gutterWidth = (0.016 * (Math.pow(ratioM.value, (-4 / 7))) * Math.pow(gutterLength.value, (3 / 28)) * Math.pow((rainfall.value * (roofWidth.value * roofLength.value)), (5 / 14)));
 
-submitBtn.addEventListener('click', gutterCalc);
+    outputText1.innerHTML = "Gutter Width: " + gutterWidth + '"';
+    outputText2.innerHTML = "Gutter Depth: " + Math.ceil(gutterWidth * ratioM.value) + '"';
+    preventDefault();
+});
 
 // Gutter Calculator
 // https://www.metalsales.us.com/wp-content/uploads/2020/01/1b-general-information-pm-2018.pdf
